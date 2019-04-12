73b8a0b61be7b5a016ff1bf8bd4f5d74d6195f17
OodizzapandaoO <gh0st1990@mail.ru>
2019-04-12 20:25:47 +0700

    работа над отображением слов

--- src/game.h
+++ src/game.h
@@ -15,3 +15,18 @@ int window_init(x_window_param_t *param,
                 int Height); //высота, ширина и т.д.
 
 void window_free(x_window_param_t *param); //освобождает ресурсы
+
+typedef struct {     //контейнер для значений
+  char **content;    //указатель на указатель
+  unsigned capacity; //общий размер выделеной памяти
+  unsigned size; //сколько объектов в нем сейчас находится
+} string_vec_t; //"контейнер
+
+int str_vec_init(string_vec_t *vec,
+                 unsigned capacity); //подготовка структуры к работе, выделение
+                                     //изначальной памяти
+int str_vec_push(string_vec_t *vec, char *value); //если хотим, что-то добавить
+void str_vec_free(string_vec_t *vec); //освобождает вектор
+char *str_vec_random(string_vec_t *vec); //выбирает рандомное слово
+int str_vec_load_from_file(string_vec_t *vec, char *filedir,
+                           char *filename); //выгружаем слова из файла
